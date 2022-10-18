5b6c20e2bbaf8f6eee2c2ecf94bfecec1e4222f6
Talantone <talgatsitd@gmail.com>
2022-10-18 13:58:13 +0200

    added comments

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from datetime import date
 
 
 def log_packer(directory='var/log'):
+    """Function to pack files of the .log format to the .tar.gz format"""
     filename = '{}/{}.tar.gz'.format(directory, str(date.today()))
     tar = tarfile.open(filename, 'w')
     for file in os.listdir(directory):
-        if file.endswith('.log'):           #rename this if you need it(or delete this if you want to pack all files)
+        if file.endswith('.log'):  # rename this if you need it(or delete this if you want to pack all files)
             tar.add('{}/{}'.format(directory, file))
             os.remove('{}/{}'.format(directory, file))
     tar.close()
 
 
 def main():
-    log_packer()                           #add argument here if you need to use other directory
+    log_packer()  # add argument here if you need to use other directory
 
 
 if __name__ == '__main__':
